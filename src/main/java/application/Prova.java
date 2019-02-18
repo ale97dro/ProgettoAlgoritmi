e339cf59635e4307dba53c0a4a698ca3a7dc0a31
Alessandro Bianchi <alex24br@yahoo.it>
2019-02-18 14:24:31 +0100

    start commit

---
+++ src/main/java/application/Prova.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Prova
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Ciao");
+    }
+}
---
+++ src/test/java/ProvaTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProvaTest
+{
+    @Test
+    public void testProva()
+    {
+        assertEquals(true, true);
+    }
+}
