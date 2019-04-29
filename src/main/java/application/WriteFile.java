896aea11e45c3d847b3e666c1e757f3da8fef075
Alessandro Bianchi <alex24br@yahoo.it>
2019-04-29 23:42:44 +0200

    file writer

---
+++ src/main/java/application/WriteFile.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class WriteFile
+{
+    public void writeFile(String fileName, Tour tour)
+    {
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+            writer.write("NAME : " + fileName + "\n");
+            writer.write("COMMENT : Optimun tour for " + fileName + "(" + tour.computeTourCost() + ")\n");
+            writer.write("TYPE : TOUR\n");
+            writer.write("DIMENSION : " + tour.getCities().size() + "\n");
+            writer.write("TOUR_SECTION\n");
+
+            for(int i = 0; i < tour.getTour().size() - 1; i++)
+                writer.write(tour.getTourCity(i) + "\n");
+
+            writer.write("-1\n");
+            writer.write("EOF\n");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
